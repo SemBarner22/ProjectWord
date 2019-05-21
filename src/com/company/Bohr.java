9b1f15b8770ec005fd654e9530b308667907221c
SemBarner22 <ildar1001@mail.ru>
2019-05-21 22:41:43 +0300

    working with lower case english words, corrects one mispelled letter with others which are nearby on the keyboard, cleaned the code a little. Answer: words are in file "possible.txt" each on a new line.

--- src/com/company/Bohr.java
+++ src/com/company/Bohr.java
@@ -5,28 +5,14 @@ import java.io.*;
 
 public class Bohr {
     FastScanner in;
-    //PrintWriter out;
-
     static int k = 1000;
     static Node root;
-
-    public Bohr() throws FileNotFoundException {
-        //new FastScanner();
-    }
 
     public void solve() {
         int n = in.nextInt();
         root = new Node();
         for (int i = 0; i < n; i++) {
-           // if (in.next().toLowerCase().compareTo("+") == 0) {
                 add(in.next());
-           /* } else {
-                if (contains(in.next().toLowerCase())) {
-                    //out.println("YES");
-                } else {
-                    //out.println("NO");
-                }
-            }*/
         }
     }
 
@@ -72,9 +58,7 @@ public class Bohr {
 
     public void run() throws FileNotFoundException {
         in = new FastScanner();
-        //out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
         solve();
-        //out.close();
     }
 
     class FastScanner {
@@ -82,7 +66,6 @@ public class Bohr {
         StringTokenizer st;
 
         public FastScanner() throws FileNotFoundException {
-            //br = new BufferedReader(new InputStreamReader(System.in));
             br = new BufferedReader(new InputStreamReader(new FileInputStream("20k.txt")));
         }
 
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,12 +9,8 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
         String word = scanner.next();
-        //File myFile = new File("word.txt");
-        //System.out.println("Attempting to read from file in: "+myFile.getCanonicalPath());
-        //try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("words.txt")))) {
             Bohr bohr = new Bohr();
             bohr.run();
-
             try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("possibleWords.txt")))) {
                 String[] a = new String[27]; //words that are nearby on the keyboard near the symbol;
                     a[0] = "qwsxz";
@@ -43,17 +39,15 @@ public class Main {
                     a[23] = "zsdc";
                     a[24] = "tghju";
                     a[25] = "asx";
-                    //System.out.println("kek");
                  for (int j = 0; j < word.length(); j++) {
                      for (int t = 0; t <  a[word.charAt(j) - 'a'].length(); t++) {
                          String s = word.substring(0, j) + a[word.charAt(j) - 'a'].charAt(t) + word.substring(j + 1);
-                         //System.out.println(s);
                          if (bohr.contains(s)) {
-                             System.out.println(s);
+                             bw.write(s);
+                             bw.newLine();
                          }
                      }
                  }
-           // }
         } catch (FileNotFoundException e) {
             System.out.println("Dictionary file is not found!");
         } catch (IOException e) {
